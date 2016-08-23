9927ad4f7c275a0495b11e19495f4dcd247fb7c3
mmmeyers <marsi286@gmail.com>
2016-08-23 17:39:01 -0400

    add roles to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,22 @@
 class User < ActiveRecord::Base
+  enum role: [:visitor, :subscriber, :admin]
   belongs_to :subscription
   has_many :boxes
   validates :username, :email, presence: true
   validates :username, :email, uniqueness: true
+  after_initialize :set_default_role, :if => :new_record?
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def set_default_role
+    self.role ||= :visitor
+  end
+
+  def boxes
+    self.subscription.boxes
+  end
+
 end
