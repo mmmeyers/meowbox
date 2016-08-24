44f2aedaf2c229109a863b4d34e3e9a24ac4e90d
mmmeyers <marsi286@gmail.com>
2016-08-24 17:49:21 -0400

    create subscription index page that links to show page for each level

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,5 +1,9 @@
 class SubscriptionsController < ApplicationController
 
+  def index
+    @subscriptions = Subscription.all
+  end
+
   def show
     @subscription = Subscription.find(params[:id])
   end
