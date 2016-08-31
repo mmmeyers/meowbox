a7e6a51d07eafc08964c55f99780529e71715cd0
mmmeyers <marsi286@gmail.com>
2016-08-31 16:03:39 -0400

    only admin can create a box

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -3,4 +3,24 @@ class BoxesController < ApplicationController
   def index
     @boxes = Box.all
   end
+
+  def new
+    @box = Box.new
+  end
+
+  def create
+    @box = Box.new(box_params)
+    if @box.save
+      redirect_to boxes_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def box_params
+      params.require(:box).permit(:title, :date_of_box)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
 
   resources :subscriptions
   resources :users
+  resources :boxes
 
 end
