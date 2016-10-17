32256bdba09bcf8ea82bb05133b10c22bb94cf9f
mmmeyers <marsi286@gmail.com>
2016-10-17 13:18:10 -0400

    set thumbnail size

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,6 @@ class Item < ActiveRecord::Base
   has_many :box_items
   has_many :boxes, through: :box_items
   validates :title, :description, :size, presence: true
-  has_attached_file :photo, default_url: ':style/default.png'
+  has_attached_file :photo, default_url: ':style/default.png', styles: { thumb: "100x100>" }
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 end
