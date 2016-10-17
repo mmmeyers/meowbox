5db1b3ba7c8a0623efd7cbc039dc79ac67dab631
mmmeyers <marsi286@gmail.com>
2016-10-17 13:12:44 -0400

    add default image

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,6 @@ class Item < ActiveRecord::Base
   has_many :box_items
   has_many :boxes, through: :box_items
   validates :title, :description, :size, presence: true
-  has_attached_file :photo
+  has_attached_file :photo, default_url: ':style/default.png'
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 end
