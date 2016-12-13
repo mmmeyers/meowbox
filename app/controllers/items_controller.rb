e95ee8078bce6225b6c892f7f0cc9e2aa7aee398
mmmeyers <marsi286@gmail.com>
2016-12-12 19:10:33 -0500

    add size & subscription to item edit page and change redirect url to box items path

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,7 +44,7 @@ class ItemsController < ApplicationController
     @item = @box.items.find(params[:id])
     @item.update_attributes(item_params)
     @item.save
-    redirect_to box_item_path(@box, @item), :notice => "Item updated"
+    redirect_to box_items_path(@box), :notice => "Item updated"
   end
 
   def destroy
