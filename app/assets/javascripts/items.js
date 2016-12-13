244468d32a312ff5947f899dae9b03e705f9e8fe
mmmeyers <marsi286@gmail.com>
2016-12-13 18:25:59 -0500

    works with new item added
    but doesn’t append this new item to the already existing array

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,19 +1,19 @@
-'use strict'
-function Item(id, title, description, size, url) {
-  this.id = id;
-  this.title = title;
-  this.description = description;
-  this.size = size;
-  this.url = url;
-}
-
-Item.prototype.urlFormatting = function () {
-  var originalUrl = response["url"];
-  if(originalUrl.includes("http") == true) {
-    return originalUrl;
-  } else if(originalUrl.includes("www") == true) {
-    return "http://" + originalUrl;
-  } else {
-    return "http://www." + originalUrl;
-  }
-}
+// 'use strict'
+// function Item(id, title, description, size, url) {
+//   this.id = id;
+//   this.title = title;
+//   this.description = description;
+//   this.size = size;
+//   this.url = url;
+// }
+//
+// Item.prototype.urlFormatting = function () {
+//   var originalUrl = response["url"];
+//   if(originalUrl.includes("http") == true) {
+//     return originalUrl;
+//   } else if(originalUrl.includes("www") == true) {
+//     return "http://" + originalUrl;
+//   } else {
+//     return "http://www." + originalUrl;
+//   }
+// }
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -48,7 +48,7 @@ class ItemsController < ApplicationController
     @item = @box.items.find(params[:id])
     @item.update_attributes(item_params)
     @item.save
-    redirect_to box_items_path(@box), :notice => "Item updated"
+    redirect_to box_path(@box), :notice => "Item updated"
   end
 
   def destroy
