84e8f1a98fd64aca4691c9aaa163606a675309e3
mmmeyers <marsi286@gmail.com>
2016-12-20 16:20:07 -0500

    move js to boxes index page

--- app/assets/javascripts/boxes.js
+++ app/assets/javascripts/boxes.js
@@ -1,9 +1,1 @@
-$(function() {
-  $(document.body).off('click', 'nav.pagination a');
-  $(document.body).on('click', 'nav.pagination a', function(e) {
-    e.preventDefault();
 
-    $("#boxes_container").load($(this).attr("href"));
-    return false;
-  });
-});
